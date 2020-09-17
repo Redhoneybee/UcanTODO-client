109db5d5667c0172059c1d85c823b355257661fa
redhoneybee <flack8801@gmail.com>
2020-09-17 17:29:08 +0900

    macro value and type

---
+++ src/components/types.js
@@ -0,0 +1,10 @@
+// default types
+export const NUMBER = "number";
+export const STRING = "string";
+
+// strings
+export const MAINTOPICINFO = "mainTopicInfo";
+export const SUBTOPICSINFO = "subTopicInfo";
+
+// MAX and MIN
+export const MAXSUBTOPICS = 20;
